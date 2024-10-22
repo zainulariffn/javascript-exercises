ae59fc0d2586256fb0dbf2226e3c08c21f50d8cc
Zainul Rashid <56968419+zainulariffn@users.noreply.github.com>
2024-10-23 00:26:35 +0800

    Finished-js-exercises-02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  if (num < 0) return "Error";
 
+  let str2 = "";
+  for (let i = 0; i < num; i++) {
+    str2 += str;
+  }
+  return str2;
 };
+repeatString("Hey", 3);
+
+// Prevent infinite loop
+// Store string
+// Create a loop
+// Store str2 inside new variable
+// Return result for each loop
 
 // Do not edit below this line
 module.exports = repeatString;
