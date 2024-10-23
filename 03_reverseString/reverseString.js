0bdf1dc032c2de0b9420013adffcaf25818cfe54
Zainul Rashid <56968419+zainulariffn@users.noreply.github.com>
2024-10-23 22:15:39 +0800

    Finished-js-exercises-03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function (strOne) {
+  let strReverseOne = strOne;
 
+  // Split the string
+  let strOneSplit = strReverseOne.split("");
+
+  // Reverse the string
+  let strOneReverse = strOneSplit.reverse("");
+  // Join the string
+  let strOneJoin = strOneReverse.join("");
+
+  return strOneJoin;
 };
+
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
