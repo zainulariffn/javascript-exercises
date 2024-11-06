a5e11cd7382382e0c6e781cc841ac754b315f1bd
Zainul Rashid <56968419+zainulariffn@users.noreply.github.com>
2024-11-07 02:01:57 +0800

    Finished-js-exercises-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (startVal, endVal) {
+  if (!Number.isInteger(startVal) || !Number.isInteger(endVal)) return "ERROR"; // Test with non-integer
+  if (startVal < 0 || endVal < 0) return "ERROR"; // Test with negative integer
+  // Test with greater starting value with swap position
+  if (startVal > endVal) {
+    const temp = startVal;
+    startVal = endVal;
+    endVal = temp;
+  }
+  let finResult = 0;
 
+  for (let i = startVal; i <= endVal; i++) {
+    finResult += i;
+  }
+  return finResult;
 };
 
 // Do not edit below this line
