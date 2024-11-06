52f6f166f68aeeec8d13c50260b1ade64ba99f5a
Zainul Rashid <56968419+zainulariffn@users.noreply.github.com>
2024-11-07 01:42:09 +0800

    Finished-js-exercises-06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (years) {
+  if (years % 4 === 0 && (years % 100 !== 0 || years % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
+leapYears(1996); // true
+leapYears(1987); // false
+leapYears(34992); // true
+leapYears(1990); // false
+leapYears(1600); // true
+leapYears(700); // false
 
 // Do not edit below this line
 module.exports = leapYears;
