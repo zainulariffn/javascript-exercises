d5a3cdee25b4a551546304bc95c3d5219233caad
Zainul Rashid <56968419+zainulariffn@users.noreply.github.com>
2025-03-22 00:26:48 +0800

    03-Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let strSplit = str.split("");
+  let reverseArray = strSplit.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
 };
+
+console.log(reverseString(`Hey there`));
 
 // Do not edit below this line
 module.exports = reverseString;
