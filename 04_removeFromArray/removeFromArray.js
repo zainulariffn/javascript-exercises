51c002709a2c77a0f7cf96117386f60ccab538ec
Zainul Rashid <56968419+zainulariffn@users.noreply.github.com>
2025-03-22 00:59:50 +0800

    04-Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...removeArr) {
+  let newArr = [];
+  arr.forEach((item) => {
+    if (!removeArr.includes(item)) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
 };
+
+console.log(
+  removeFromArray([1, 2, 2, 3, 4, "Tacos", 7], 3, 2, "Tacos", 7, "1")
+);
 
 // Do not edit below this line
 module.exports = removeFromArray;
+// const removeFromArray = function (arr, removeArr, ...theArggs) {
+//   let arrRem = "";
+//   arr = arrRem;
+//   let removeArray = removeArr.push();
+//   console.log(rem);
+//   return removeArray;
+// };
