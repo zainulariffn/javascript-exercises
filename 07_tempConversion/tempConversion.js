37a0a6ac4819059eaa7205687cc09c51af7c64ac
Zainul Rashid <56968419+zainulariffn@users.noreply.github.com>
2025-03-22 21:02:43 +0800

    07-Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (numFah) {
+  let conv = ((numFah - 32) * 5) / 9;
+  let round = Math.round(conv * 10) / 10;
+  return round;
 };
+console.log(convertToCelsius(32));
+console.log(convertToCelsius(100));
+console.log(convertToCelsius(-100));
 
-const convertToFahrenheit = function() {
+// //
+// //
+// //
+
+const convertToFahrenheit = function (numCel) {
+  let conv = (numCel * 9) / 5 + 32;
+  let round = Math.round(conv * 10) / 10;
+  return round;
 };
+console.log(convertToFahrenheit(0));
+console.log(convertToFahrenheit(73.2));
+console.log(convertToFahrenheit(-10));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
