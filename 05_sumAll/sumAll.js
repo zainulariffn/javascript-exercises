da69a65ff10f1a2842ed319b23ab7596561ebb10
Zainul Rashid <56968419+zainulariffn@users.noreply.github.com>
2025-03-22 16:27:41 +0800

    05-Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (startVal, endVal) {
+  if (!Number.isInteger(startVal) || !Number.isInteger(endVal)) return `ERROR`;
+  if (startVal < 0 || endVal < 0) return `ERROR`;
+  if (startVal > endVal) {
+    const temp = startVal;
+    startVal = endVal;
+    endVal = temp;
+  }
+  let totalVal = 0;
+  for (i = startVal; i <= endVal; i++) {
+    totalVal += i;
+  }
+  return totalVal;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
